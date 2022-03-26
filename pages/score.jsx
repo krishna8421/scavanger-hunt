e189b24e82b6ed2f8284a0fe243e261c5ef16951
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-03-27 02:55:48 +0530

    Score

--- pages/score.jsx
+++ pages/score.jsx
@@ -31,9 +31,27 @@ const Score = () => {
     <div className="flex  flex-col overflow-hidden  text-slate-200 items-center background-main w-screen min-h-screen pt-12">
       {userDataArray.map((user, i) => {
         return (
-          <div key={i} className="flex w-[25rem] mb-16 items-center">
+          <div
+            key={i}
+            className={`flex sm:w-[30rem] w-11/12 mb-16 items-center p-6 ${
+              i === 0 || i === 1 || i === 2
+                ? "text-3xl font-semibold border border-slate-700 rounded-xl "
+                : ""
+            } ${
+              i === 0
+                ? "bg-[#C9B037]"
+                : i === 1
+                ? "bg-[#f2f3f4] text-black"
+                : i === 2
+                ? "bg-[#9f6C36]"
+                : ""
+            }`}
+          >
+            <h1 className="font-Oxygen mr-2 flex-1">
+              {i === 0 ? `${i + 1}st` : i === 1 ? `${i + 1}nd` : i === 2 ? `${i + 1}rd` : i + 1}
+            </h1>
             <h1 className="font-Oxygen mr-8 flex-1">{user.name}</h1>
-            <h1 className="text-lg text-center flex-1">{user.score}</h1>
+            <h1 className="text-center flex-1">{user.score}</h1>
           </div>
         );
       })}
